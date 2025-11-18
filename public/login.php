d795b50c21a65b07283696ccaf24552702fc9cca
Promise267 <promrizal@gmail.com>
2025-11-18 12:56:45 +1030

    Fix the session issue

--- public/login.php
+++ public/login.php
@@ -2,7 +2,6 @@
 if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
-session_start();
 require_once __DIR__ . '/../app/controllers/UserController.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $ctrl = new UserController();
