bec6caabaf576e64b8c58b43083c7c6fda9f77ee
Denis Maingi <dantezwaitley@gmail.com>
2025-11-18 12:44:12 +1030

    Fix session handling and update file path for UserController in login.php

--- public/login.php
+++ public/login.php
@@ -1,6 +1,9 @@
 <?php
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
 session_start();
-require_once '/../app/controllers/UserController.php';
+require_once __DIR__ . '/../app/controllers/UserController.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $ctrl = new UserController();
     $user_id = $ctrl->loginUser($_POST['email'], $_POST['password']);
